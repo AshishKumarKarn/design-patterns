fcec84b0ebbedd06c629dec53d63f54b5afbf9a0
Ashish Karn <ashishkrkarn@gmail.com>
2024-04-20 21:38:44 +0530

    CovarianceAndContravariance

---
+++ gof_design_patterns/src/main/java/com/karn/other/CovarianceAndContravariance.java
@@ -0,0 +1,49 @@
+package com.karn.other;
+
+public class CovarianceAndContravariance {
+}
+
+/**
+ * Covariance
+ * Supertype accepts more general input than subtype
+ * if A:B then I<A>:I<B>, if A is subtype of B, then class using A should be subtype be class using B
+ */
+class FruitSequence {
+    Fruit next() {
+        return new Fruit();
+    }
+}
+
+class OrangeSequence extends FruitSequence {
+    @Override
+    Orange next() {
+        return new Orange();
+    }
+}
+
+class Fruit {
+
+}
+
+class Orange extends Fruit {
+
+}
+
+/**
+ * Contravariance
+ * Subtype accepts more general output than super type
+ * if A:B then I<B>:I<A>, if A is subtype of B, then class using B should be subtype be class using A
+ */
+class OrangeJuicer {
+    void juice(Orange m) {
+
+    }
+}
+
+class FruitJuicer extends OrangeJuicer {
+
+    void juice(Fruit f) {
+
+    }
+
+}
